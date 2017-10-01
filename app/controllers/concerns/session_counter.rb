100f02e863b60f173699d052253a94e9231b92c3
crunchysoul <zzzcca12@gmail.com>
2017-10-02 05:54:32 +1100

    Add cart - chapter 9

---
+++ app/controllers/concerns/session_counter.rb
@@ -0,0 +1,9 @@
+module SessionCounter
+
+  private
+
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
+  end
+end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
+  include SessionCounter
+
   def index
     @products = Product.order(:title)
+    @counter = increment_count
   end
 end
