21994fa336f5ab6861a8222363e2fa04d6a585db
crunchysoul <zzzcca12@gmail.com>
2017-10-02 13:39:33 +1100

    Update smartcart: - Adding a column to an existing table, with a default value - Migrating existing data into the new table format - Providing a flash notice of an error that was detected - Using the logger to log events - Removing a parameter from the permitted list - Deleting a record - Adjusting the way a table is rendered, using CSS

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,14 +27,12 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-    # reset session counter when customer add to cart
+    @line_item = @cart.add_product(product)
     session[:counter] = 0
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-                      notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -68,13 +66,13 @@ class LineItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
+  end
 end
